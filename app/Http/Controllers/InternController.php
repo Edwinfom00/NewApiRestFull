999dd0cdc3847fa3054922c366674c80b5b98d7a
Edwin Fom <edwinfom05@gmail.com>
2024-01-24 03:48:20 +0100

    modified

---
+++ app/Http/Controllers/InternController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\InternFormRequest;
+use App\Models\Intern;
+use Illuminate\Http\Request;
+
+class InternController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $intern = Intern::all();
+        return response()->json($intern);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(InternFormRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $intern = Intern::create($validatedData);
+
+        return response()->json($intern, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(InternFormRequest $request, Intern $intern)
+    {
+        $validatedData = $request->validated();
+
+        $intern = Intern::update($validatedData);
+
+        return response()->json($intern, 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Intern $intern)
+    {
+        $intern->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\InternController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/intern', [InternController::class,'index']);
+Route::post('/intern', [InternController::class, 'store']);
+Route::get('/intern/{intern}', [InternController::class, 'show']);
+Route::put('/intern/{intern}', [InternController::class, 'update']);
+Route::delete('/intern/{intern}', [InternController::class, 'destroy']);
